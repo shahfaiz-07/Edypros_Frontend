2632b11542c6c047f3d404c989128b6b2a79a395
S Faizaan Hussain <hussainfaizaan07@gmail.com>
2024-07-05 20:57:25 +0530

    modified apiconnector

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export const axiosInstance = axios.create({});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
+    console.log(import.meta.env)
     return axiosInstance({
+        baseURL: import.meta.env.VITE_API_BASE_URL,
         method,
         url,
         data: bodyData ? bodyData : null,
