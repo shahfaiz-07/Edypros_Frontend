c6eccfb8587391f879fc0ca54f2b075d4f4f341a
S Faizaan Hussain <hussainfaizaan07@gmail.com>
2024-07-05 22:04:11 +0530

    modified api requests for development as well as production

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL
+    baseURL: import.meta.env.PROD ? import.meta.env.VITE_API_BASE_URL : ""
 });
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
