91c9aae867529a6002101bb77a6b696287416106
S Faizaan Hussain <hussainfaizaan07@gmail.com>
2024-07-05 20:23:42 +0530

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'https://edypros-backend.onrender.com',
+        changeOrigin: true,
+      },
+    },
+  }
 //   server: {
 //     proxy: {
 //         '/api/v1': '',
